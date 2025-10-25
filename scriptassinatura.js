ba1425ecc321edd5c813e0af4159283cb40c5e63
salamaleicogames <cl204421@g.unicamp.br>
2025-10-25 17:14:15 -0300

    0.6

--- scriptassinatura.js
+++ scriptassinatura.js
@@ -2,25 +2,3 @@
 //Scroll Trigger
 import gsap from "https://cdn.skypack.dev/gsap";
   import { ScrollTrigger } from "https://cdn.skypack.dev/gsap/ScrollTrigger";
-
-  gsap.registerPlugin(ScrollTrigger);
-  
-
-const mainImg = document.getElementById('.im2sticky');
-    const textBlocks = document.querySelectorAll('.texto');
-
-    textBlocks.forEach(block => {
-      ScrollTrigger.create({
-        trigger: block,
-        start: "top center",
-        end: "bottom center",
-        onEnter: () => {
-          const src = block.getAttribute('data-img');
-          mainImg.src = src; // troca a imagem
-        },
-        onEnterBack: () => {
-          const src = block.getAttribute('data-img');
-          mainImg.src = src; // troca a imagem ao subir
-        }
-      });
-    });
