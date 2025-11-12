ecf9b4e1258ff9af1151776417995639ea4b6478
salamaleicogames <cl204421@g.unicamp.br>
2025-11-12 01:13:24 -0300

    0.6

--- scriptassinatura.js
+++ scriptassinatura.js
@@ -2,3 +2,17 @@
 //Scroll Trigger
 import gsap from "https://cdn.skypack.dev/gsap";
   import { ScrollTrigger } from "https://cdn.skypack.dev/gsap/ScrollTrigger";
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visivel');
+    }
+  });
+});
+
+document.querySelectorAll('.Network3').forEach((el) => observer.observe(el));
+document.querySelectorAll('p').forEach((el) => observer.observe(el));
+
+
